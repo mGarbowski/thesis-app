30bd44450a945c47a9d43a86d2872193e1bad715
mgarbowski <mikolaj.garbowski@gmail.com>
2025-09-23 20:59:59 +0200

    feat: make sql alchemy logging configurable

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_prefix="")
     database_url: str
     cors_allowed_origin: str
+    enable_sqlalchemy_logging: bool = True
 
 
 settings = Settings()
