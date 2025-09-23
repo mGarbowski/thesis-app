04a013470bddf1b17a859b6a8f283eb759850955
mgarbowski <mikolaj.garbowski@gmail.com>
2025-09-23 18:00:01 +0200

    feat: add TODO

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
+# TODO move to config file
 DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/face_recognition"
 engine = create_async_engine(DATABASE_URL)
 async_session = sessionmaker(engine, class_=AsyncSession)
