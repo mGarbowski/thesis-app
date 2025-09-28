654b299a2d08171e439cacdf63f547a4738852fc
mgarbowski <mikolaj.garbowski@gmail.com>
2025-09-28 16:02:10 +0200

    feat: change index type

--- database/init.sql
+++ database/init.sql
@@ -13,7 +13,13 @@ CREATE TABLE face_images (
 );
 
 -- Vector similarity index
-CREATE INDEX ON face_images USING ivfflat (feature_vector vector_cosine_ops);
+-- https://github.com/pgvector/pgvector?tab=readme-ov-file#indexing
+-- Index is approximate, trade-off between speed and recall
+-- Search with no index provides perfect recall
+-- For now, I did not encounter an issue with hnsw index, ivfflat on the other hand is not working well
+-- I am leaving this index for now as it proivides a 10x speedup
+-- It may need removing and falling back to full table scan if it causes issues
+CREATE INDEX ON face_images USING hnsw (feature_vector vector_cosine_ops);
 
 -- Regular indexes
 CREATE INDEX idx_face_images_created_at ON face_images(created_at);
